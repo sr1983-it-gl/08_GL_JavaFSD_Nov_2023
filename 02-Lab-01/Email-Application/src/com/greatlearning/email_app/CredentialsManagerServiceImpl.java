2155bf9e6ad767edab75f17688413efdaa4ea51b
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-12-03 17:09:27 +0530

    Lab-02 - Implementation email-address-generator - part I

--- 02-Lab-01/Email-Application/src/com/greatlearning/email_app/CredentialsManagerServiceImpl.java
+++ 02-Lab-01/Email-Application/src/com/greatlearning/email_app/CredentialsManagerServiceImpl.java
@@ -6,8 +6,21 @@ public class CredentialsManagerServiceImpl
 	@Override
 	public String generateEmailAddress(
 			String firstName, String lastName, int departmentCode) {
-		// TODO Auto-generated method stub
-		return null;
+				
+		StringBuilder emailAddressBuilder =
+			new StringBuilder();
+		
+		emailAddressBuilder.append(firstName);
+		emailAddressBuilder.append(".");
+		emailAddressBuilder.append(lastName);
+		
+		// TODO - Include @, departmentName, 'gl.com'
+		
+		// Department name from department code		
+			// Use of if-else
+			// switch case
+		
+		return emailAddressBuilder.toString();
 	}
 
 	@Override
