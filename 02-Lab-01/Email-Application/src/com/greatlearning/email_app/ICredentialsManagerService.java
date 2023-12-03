a55f4b1d7c06d44b1fa90cf9a18721f6684dab6b
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-12-03 16:57:59 +0530

    Lab-02 - Adding operations to interface

--- 02-Lab-01/Email-Application/src/com/greatlearning/email_app/ICredentialsManagerService.java
+++ 02-Lab-01/Email-Application/src/com/greatlearning/email_app/ICredentialsManagerService.java
@@ -2,7 +2,9 @@ package com.greatlearning.email_app;
 
 public interface ICredentialsManagerService {
 
-	String generateEmailAddress(String firstName, String lastName);
+	String generateEmailAddress(
+		String firstName, String lastName, 
+		int departmentCode);
 
 	String generatePassword();
 	
