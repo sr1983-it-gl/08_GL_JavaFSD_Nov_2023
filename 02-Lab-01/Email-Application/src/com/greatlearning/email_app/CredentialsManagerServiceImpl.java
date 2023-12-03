2a621cc89285795525d090bfac524cbe5926fc0e
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-12-03 17:02:18 +0530

    Lab-02 - Create class implementing interface

---
+++ 02-Lab-01/Email-Application/src/com/greatlearning/email_app/CredentialsManagerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.greatlearning.email_app;
+
+public class CredentialsManagerServiceImpl 
+	implements ICredentialsManagerService {
+
+	@Override
+	public String generateEmailAddress(
+			String firstName, String lastName, int departmentCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generatePassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void displayGeneratedCredentials() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
