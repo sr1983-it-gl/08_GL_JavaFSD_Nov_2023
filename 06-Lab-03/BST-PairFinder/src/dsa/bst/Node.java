5b1f2b9edd0619974042c154d15bc17b87a37f25
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-04 15:51:47 +0530

    BST-Node Class

---
+++ 06-Lab-03/BST-PairFinder/src/dsa/bst/Node.java
@@ -0,0 +1,31 @@
+package dsa.bst;
+
+public class Node {
+
+	Integer data;
+	Node left;
+	Node right;
+	
+	public Integer getData() {
+		return data;
+	}
+	public void setData(Integer data) {
+		this.data = data;
+	}
+	public Node getLeft() {
+		return left;
+	}
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	public Node getRight() {
+		return right;
+	}
+	public void setRight(Node right) {
+		this.right = right;
+	}
+	
+	public String toString() {
+		return String.format("[%d]", data);
+	}
+}
