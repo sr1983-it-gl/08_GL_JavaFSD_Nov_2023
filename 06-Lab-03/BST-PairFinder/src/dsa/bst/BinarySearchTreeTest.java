a6c878006364eb2498ddd17f26ea18e06a3d7b64
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-04 16:51:20 +0530

    BST - Sample Tree

---
+++ 06-Lab-03/BST-PairFinder/src/dsa/bst/BinarySearchTreeTest.java
@@ -0,0 +1,12 @@
+package dsa.bst;
+
+public class BinarySearchTreeTest {
+
+	public static void main(String[] args) {
+		
+		BinarySearchTree bst 
+			= BSTUtils.sampleBST();
+		
+//		bst.get
+	}
+}
