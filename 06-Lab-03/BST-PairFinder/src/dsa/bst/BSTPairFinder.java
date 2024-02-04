d1371d799bfea96e87fc549773359294970680c2
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-04 17:12:13 +0530

    BST - Pair Finder - v1

---
+++ 06-Lab-03/BST-PairFinder/src/dsa/bst/BSTPairFinder.java
@@ -0,0 +1,15 @@
+package dsa.bst;
+
+public class BSTPairFinder {
+
+	BinarySearchTree bst;
+	Integer sum;
+	
+	public BSTPairFinder(BinarySearchTree bst, Integer sum) {
+		
+		this.bst = bst;
+		this.sum = sum;
+	}
+	
+	
+}
