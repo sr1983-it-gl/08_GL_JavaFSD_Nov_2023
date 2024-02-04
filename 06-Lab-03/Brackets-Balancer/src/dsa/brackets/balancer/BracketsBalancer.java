8a6279c8c52e0335549f86f83a8fb41c8a9f69f9
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-04 18:42:24 +0530

    Brackets Balancer - Pseudo code

--- 06-Lab-03/Brackets-Balancer/src/dsa/brackets/balancer/BracketsBalancer.java
+++ 06-Lab-03/Brackets-Balancer/src/dsa/brackets/balancer/BracketsBalancer.java
@@ -1,6 +1,8 @@
 package dsa.brackets.balancer;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 public class BracketsBalancer {
 
@@ -13,20 +15,72 @@ public class BracketsBalancer {
 	
 	public void check() {
 		
-		// Stack<Character> stack;
+		
+		 Stack<Character> stack = new Stack<>();
 		
 		//  (  [  [  {  }  ]  ]  )
 		
 		// Visit each character from the expression
 		
 		// for-character
-			// open-bracket
-				// push()
 			// close-bracket
+	
+		int length = expression.length();
+		
+		if (length %2 != 0) {
+			System.out.println("Odd number of character - Invalid Expression");
+			return;
+		}
+		
+		for (int index = 0; index < length; index ++) {
+			
+			Character visitedChar = expression.charAt(index);
+			
+			if (getOpenBrackets().contains(visitedChar)) {
+				
+				// open-bracket
+				// push()
+				
+				stack.push(visitedChar);
+				
+			}else if (getClosedBrackets().contains(visitedChar)) {
+				
 				// pop()
 				// check between
 					// popped-out item [open-bracket-char] // {
 					// visited item [closed-bracket-char // }
+				
+				Character poppedItem = stack.pop();
+				
+				Character assumedOpenBracketChar = poppedItem;
+				Character assumedClosedBracketChar = visitedChar;
+				
+				Character openBracketChar 
+					= getOpenBracketChar(assumedClosedBracketChar);
+				
+				if (assumedOpenBracketChar == openBracketChar) {
+					
+					// Match
+					continue;
+				}else {
+					System.out.println("Non-matching Bracket");
+					break;
+				}
+			}else {
+				
+				System.out.println("Invalid characters encountered :" 
+						+ visitedChar);
+				break;
+			}
+		}
+		
+		if (stack.isEmpty()) {
+			
+			System.out.println("Bracket Expression Valid");
+		}else {
+			System.out.println("Bracket Expression Invalid");
+		}
+		
 	}
 	
 	Set<Character> getOpenBrackets(){
@@ -35,16 +89,56 @@ public class BracketsBalancer {
 		// HashSet -> No Order
 		// <, {, [, (
 		
+		Set<Character> openBracketSet = new HashSet<>();
 		
+		openBracketSet.add('<');
+		openBracketSet.add('{');
+		openBracketSet.add('[');
+		openBracketSet.add('(');
+		
+		return openBracketSet;
 	}
 	
 	Set<Character> getClosedBrackets(){
+				
+		Set<Character> closeBracketSet = new HashSet<>();
+		
+		closeBracketSet.add('>');
+		closeBracketSet.add('}');
+		closeBracketSet.add(']');
+		closeBracketSet.add(')');
+		
+		return closeBracketSet;
 		
 		// >, ], }, )
 	}
 	
-	Character getClosedBracketChar(Character openBracketChar) {
+	Character getOpenBracketChar(Character closedBracketChar) {
 		
 		
+		if (closedBracketChar == '>') {
+			return '<';			
+		}else if (closedBracketChar == '}') {
+			return '{';			
+		}else if (closedBracketChar == ']') {
+			return '[';
+		}else if (closedBracketChar == ')') {
+			return '('; 
+		}
+		return ' ';
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		BracketsBalancer balancer 
+			//  {[()]}
+			//  ({[<>]})
+			//  [<<]
+			//  [<>]]
+			//  [<#|>]
+			= new BracketsBalancer("[<#|>]");
+		
+		balancer.check();
 	}
 }
