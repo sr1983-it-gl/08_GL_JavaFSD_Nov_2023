d2ed15fa07ea7d0f4380ca044d83585d0276318a
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-02-04 16:12:48 +0530

    BST-Node Class

---
+++ 06-Lab-03/BST-PairFinder/src/dsa/bst/BinarySearchTree.java
@@ -0,0 +1,29 @@
+package dsa.bst;
+
+public class BinarySearchTree {
+
+	Node root;
+	
+	void insert(int value) {
+		
+		// Root is NULL
+			// Create the new-node
+			// Make root to point to new-node
+		
+		// Root is NOT Null
+			
+			// Create the new-node
+			// Call the constructNodeDetails
+			
+			// Set the relationshiip between parent-node and the new-node
+			// Left - Child / Right-Child
+			
+	}
+	
+	NodeInsertionDetails constructNodeDetails(){
+		
+		NodeInsertionDetails nid = new NodeInsertionDetails();
+		
+		return nid;
+	}
+}
---
+++ 06-Lab-03/BST-PairFinder/src/dsa/bst/NodeInsertionDetails.java
@@ -0,0 +1,20 @@
+package dsa.bst;
+
+public class NodeInsertionDetails {
+
+	Node parentNode;
+	NodeDirection direction;
+	
+	public Node getParentNode() {
+		return parentNode;
+	}
+	public void setParentNode(Node parentNode) {
+		this.parentNode = parentNode;
+	}
+	public NodeDirection getDirection() {
+		return direction;
+	}
+	public void setDirection(NodeDirection direction) {
+		this.direction = direction;
+	}
+}
