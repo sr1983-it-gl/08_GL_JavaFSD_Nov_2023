9a9a29998a05a8aa7fd21f1fa92b17baaa6cc102
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-04-28 15:57:18 +0530

    Lab-06

---
+++ 12-Lab-06/ssrs/src/test/java/com/greatleaning/ssrs/SsrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatleaning.ssrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
